979a4ce401f5202ba049e70383e5b64e35bb72fb
Martin Zeller <mz.bremerhaven@gmail.com>
2015-01-31 01:43:56 +0100

    Handle packets on client

--- static/chat.js
+++ static/chat.js
@@ -11,10 +11,41 @@ socket.onerror = function(error)
 	alert("WebSocket error: " + error);
 }
 
-socket.onmessage = function(msg)
+socket.onmessage = function(packet)
 {
-	var cont = document.getElementById("chatbox-content")
-	cont.innerHTML = cont.innerHTML + msg.data + "<br />" ;
+	var cont = document.getElementById("chatbox-content");
+	var split = packet.data.split(" ");
+
+	var action = split[0];
+	if (action == "MSG")
+	{
+		var nick = split[1];
+		var msg = split[2];
+
+		if (nick != undefined && msg != undefined)
+		{
+			cont.innerHTML = cont.innerHTML + "&lt;" + nick + "&gt; " + msg + "<br />";
+		}
+	}
+	else if (action == "NICK")
+	{
+		var oldNick = split[1];
+		var newNick = split[2];
+
+		if (oldNick != undefined && newNick != undefined)
+		{
+			cont.innerHTML = cont.innerHTML + "* " + oldNick + " is now known as " + newNick + "<br />";
+		}
+	}
+	else if (action == "QUIT")
+	{
+		var nick = split[1];
+
+		if (nick != undefined)
+		{
+			cont.innerHTML = cont.innerHTML + "* " + nick + " has quit<br />";
+		}
+	}
 }
 
 function sendMessage()
